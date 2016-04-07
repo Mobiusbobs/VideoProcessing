5de4ec613d9e4464f06d57b284c136193162f6f0
Ray Shih <mnf.shih@gmail.com>
2016-04-07 18:59:43 +0800

    merge code from main project

---
+++ core/src/main/java/com/mobiusbobs/videoprocessing/core/Util.java
@@ -0,0 +1,29 @@
+package com.mobiusbobs.videoprocessing.core;
+
+import android.util.Log;
+
+import java.io.File;
+
+
+public class Util {
+    private static final String TAG = "Util";
+
+    private static long startTime = 0;
+
+    public static void fileSize(String filePath) {
+        File file = new File(filePath);
+        long fileSize = file.length();
+        Log.d(TAG, "fileSize=" + fileSize + ", " + fileSize / 1024 + "KB, " + fileSize / 1024 / 1024 + "MB");
+    }
+
+    public static void startTimer() {
+        startTime = System.currentTimeMillis();
+        Log.d(TAG, "TEST: start timer!");
+    }
+
+    public static void endTimer(String msg) {
+        long endTime = System.currentTimeMillis();
+        Log.d(TAG, "TEST: " + msg + ": time elapse = " + (endTime - startTime));
+    }
+
+}
