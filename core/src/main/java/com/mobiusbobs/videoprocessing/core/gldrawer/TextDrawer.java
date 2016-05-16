c287e6a7857f5418fbeec110380e0d0293d48ef3
Albert Wang <albert@mobiusbobs.com>
2016-05-16 14:38:40 +0800

    -fix textfont/margin by converting dp to pixels since drawBitmap use pixel

--- core/src/main/java/com/mobiusbobs/videoprocessing/core/gldrawer/TextDrawer.java
+++ core/src/main/java/com/mobiusbobs/videoprocessing/core/gldrawer/TextDrawer.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 
 import com.mobiusbobs.videoprocessing.core.util.CoordConverter;
 
@@ -28,6 +30,7 @@ public class TextDrawer implements GLDrawable {
   private CoordConverter converter;
   private String text;
   private int resId;
+  private float scale;
 
   // Constructor
   public TextDrawer(Context context, CoordConverter converter, int videoWidth, String text, int resId) {
@@ -37,19 +40,28 @@ public class TextDrawer implements GLDrawable {
     this.text = text;
     this.resId = resId;
 
+    scale = context.getResources().getDisplayMetrics().density;
     stickerDrawer = new BaseDrawer();
   }
 
   public void init() throws IOException {
+    int marginTop = (int) (10 * scale);
+
     Bitmap bitmap = generateBitmap(text, resId);
-    stickerDrawer.setVerticesCoordinate(converter.getAlignTopRightVertices(bitmap));
+    stickerDrawer.setVerticesCoordinate(converter.getAlignTopVertices(bitmap, marginTop));
     stickerDrawer.init(bitmap);
     bitmap.recycle();
   }
 
   public Bitmap generateBitmap(String text, int resId) {
-    float rightMargin = 15;
-    int textShift = 5;
+    float spaceRightMargin = 10.0f;
+    float iconRightMargin = 2.0f;
+    float textFontSize = 13.0f;
+
+    // convert dp to pixel
+    int spaceRightMarginPx = (int) (spaceRightMargin * scale + 0.5f);
+    int iconRightMarginPx = (int) (iconRightMargin * scale + 0.5f);
+    int textSizeInPixel = (int) (textFontSize * scale + 0.5f);
 
     // create paw bitmap
     Bitmap icon = BitmapFactory.decodeResource(context.getResources(), resId);
@@ -57,16 +69,16 @@ public class TextDrawer implements GLDrawable {
     // create the bitmap to draw on
     int iconHeight = icon.getHeight();
     int containerHeight = iconHeight * 2;
-    Bitmap bitmap = Bitmap.createBitmap(videoWidth, containerHeight, Bitmap.Config.ARGB_4444);
+    Bitmap container = Bitmap.createBitmap(videoWidth, containerHeight, Bitmap.Config.ARGB_4444);
 
     // get a canvas to paint over the bitmap
-    Canvas canvas = new Canvas(bitmap);
-    bitmap.eraseColor(0);
+    Canvas canvas = new Canvas(container);
+    container.eraseColor(0);
 
     // setup paint
     Rect textBounds = new Rect();
     Paint textPaint = new Paint();
-    textPaint.setTextSize(50);
+    textPaint.setTextSize(textSizeInPixel);
     textPaint.setAntiAlias(true);
     textPaint.setARGB(0xFF, 0xFF, 0xFF, 0xFF);
     textPaint.getTextBounds(text, 0, text.length(), textBounds);
@@ -76,18 +88,18 @@ public class TextDrawer implements GLDrawable {
     float textHeight = textBounds.height();
     canvas.drawText(
         text,
-        videoWidth - (textLength + rightMargin),
-        (containerHeight + textHeight) / 2 - textShift,
+        videoWidth - (textLength + spaceRightMarginPx),
+        (containerHeight + textHeight) / 2,
         textPaint
     );
 
     // draw paw image
     if (text.length() > 0) {
-      float left = videoWidth - (icon.getWidth() + textLength + rightMargin);
+      float left = videoWidth - (icon.getWidth() + textLength + spaceRightMarginPx + iconRightMarginPx);
       canvas.drawBitmap(icon, left, (containerHeight - icon.getHeight()) / 2, null);
     }
 
-    return bitmap;
+    return container;
   }
 
   public void draw(long timeMs) {
