3cbc221b1f96c15395246922a54604b04cbf2a9a
Albert Wang <albert@mobiusbobs.com>
2016-06-15 17:16:17 +0800

    refactor based on review

--- core/src/main/java/com/mobiusbobs/videoprocessing/core/codec/TextureMovieEncoder.java
+++ core/src/main/java/com/mobiusbobs/videoprocessing/core/codec/TextureMovieEncoder.java
@@ -90,7 +90,7 @@ public class TextureMovieEncoder implements Runnable {
 
     // presentation timestamp offset
     private PTHelper ptHelper = new PTHelper();
-    private long previousPT = 0;
+    private long previousPT = -1;
 
     // Constructor
     public TextureMovieEncoder(MuxerWrapper muxerWrapper) {
@@ -369,11 +369,7 @@ public class TextureMovieEncoder implements Runnable {
         if (isRecording) {
             long currentPT = timestampNanos - ptHelper.getTimeOffsetNs();
             // block out duplicated time or wrong timestamp
-            if (previousPT != 0 && previousPT >= currentPT) {
-                if (previousPT > currentPT)
-                    Log.e(TAG, "Block out invalid timestamp " + previousPT + " > " + currentPT);
-                return;
-            }
+            if (previousPT >= currentPT) return;
             previousPT = currentPT;
 
             mInputWindowSurface.setPresentationTime(currentPT);
