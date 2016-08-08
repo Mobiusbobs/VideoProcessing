15dad61a10043daf202634a523e542f4413dbdce
colorhaake <colorhaake@gmail.com>
2016-08-08 16:31:20 +0800

    fix audio timestamp issue

--- core/src/main/java/com/mobiusbobs/videoprocessing/core/VideoProcessor.java
+++ core/src/main/java/com/mobiusbobs/videoprocessing/core/VideoProcessor.java
@@ -129,8 +129,8 @@ public class VideoProcessor {
     int outputVideoTrack = -1;
     int outputAudioTrack = -1;
 
-    // for audio hotfix
-    long lastAudioPresentationTime = 0;
+    // for audio hotfix on audio muxer
+    long audioPresentationTimeUsLast = 0;
 
     // Constructor
     private VideoProcessor() {}
@@ -658,10 +658,6 @@ public class VideoProcessor {
         ByteBuffer encoderInputBuffer = audioEncoderInputBuffers[encoderInputBufferIndex];
         int size = audioDecoderOutputBufferInfo.size;
         long presentationTime = audioDecoderOutputBufferInfo.presentationTimeUs + pTimeOffset;
-        if (presentationTime < lastAudioPresentationTime) {
-            presentationTime = lastAudioPresentationTime + 1;
-        }
-        lastAudioPresentationTime = presentationTime;
 
         if (size >= 0) {
             ByteBuffer decoderOutputBuffer =
@@ -818,6 +814,12 @@ public class VideoProcessor {
             audioEncoder.releaseOutputBuffer(encoderOutputBufferIndex, false);
             return false;
         }
+
+        long audioPresentationTimeUs = audioEncoderOutputBufferInfo.presentationTimeUs;
+        if (audioPresentationTimeUsLast >= audioPresentationTimeUs) {
+            audioEncoderOutputBufferInfo.presentationTimeUs = audioPresentationTimeUsLast + 1;
+        }
+        audioPresentationTimeUsLast = audioPresentationTimeUs;
 
         if (audioEncoderOutputBufferInfo.size != 0) {
             Log.d(TAG, "write Audio sample to muxer");
